a51b2127ec691088040f9c399e55b9fd246586d9
Matthew Lister <mdlister24@hotmail.com>
2020-02-19 21:31:30 +0000

    Changed map code to easier js and place for maps to display through HTML and css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,10 @@
-$(document).ready(function() {
-let map;
-let myMarker;
-let myLatLng;
-})
-
-// initialises Google Maps
-
-function initializeGMap(lat, lng) {
-    myLatLng = {lat, lng};
-    
-    
-//controls the type of maps used and zoom
-    var myOptions = {
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: myLatLng 
-        
-    };
-
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
-
-function mapMarker(map) {
-    myMarker = new google.maps.Marker({
-                map : map,
-              	position : myLatLng
-            }); // Set the first marker on the map
-}
-
-function addMarker(destinations, map) {
-    for (var i = 0; i < destinations.length; i++) {
-      myMarker = new google.maps.Marker({
-        position: new google.maps.LatLng(destinations[i].lat, destinations[i].lng),
-        title: destinations [i].name,
-        draggable: true,
-        map: map,
-        icon: destinations[i].icon
-      });
-    }
-  }
