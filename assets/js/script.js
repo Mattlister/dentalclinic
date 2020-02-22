783054d6f16eda3f3ec43efa6c2e2eb6261dcfb8
Matthew Lister <mdlister24@hotmail.com>
2020-02-22 11:45:51 +0000

    Added collapsible table

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,12 @@ function initMap() {
   // The marker, positioned at Bigfoot's location
   var marker = new google.maps.Marker({position: Barnsley, map: map});
 }
+
+ $("form :input").each(function(index, elem) {
+    var eId = $(elem).attr("id");
+    var label = null;
+    if (eId && (label = $(elem).parents("form").find("label[for="+eId+"]")).length == 1) {
+        $(elem).attr("placeholder", $(label).html());
+        $(label).remove();
+    }
+ });
